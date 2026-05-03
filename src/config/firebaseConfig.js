99ff6de07051e0a797748621a50fcf766903d2ee
AMULRAI <amulrai33@gmail.com>
2026-05-03 19:45:33 +0530

    feat: initialize Firebase app, authentication with persistence, and Firestore instance

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -5,13 +5,13 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDqc52UiAHkyuR6toNwKX894mshWocnf34",
-  authDomain: "movienight-app-2e8b1.firebaseapp.com",
-  projectId: "movienight-app-2e8b1",
-  storageBucket: "movienight-app-2e8b1.firebasestorage.app",
-  messagingSenderId: "518101468778",
-  appId: "1:518101468778:web:769fbdbdf8c7960cc62c13",
-  measurementId: "G-X6WHTHQE9N"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase App
